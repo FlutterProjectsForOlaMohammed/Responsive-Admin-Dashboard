69be177d696398f0b8983f51cfb3006ca0088ab5
Ola2024Mohammed <hawaiiict2022@gmail.com>
2024-08-14 18:06:08 +0300

    create Text Styles Class & Color Palette .

---
+++ lib/core/utils/color_palette.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorPalette {
+  static const darkBlue = Color(0xff064061);
+
+  static const lightBlue = Color(0xff4EB7F2);
+
+  static const mediumBlue = Color(0xff208CC8);
+
+  static const white = Color(0xffFFFFFF);
+
+  static const lightGrey = Color(0xffFAFAFA);
+
+  static const grey = Color(0xffAAAAAA);
+
+  static const black = Color(0xff292D32);
+
+  static const green = Color(0xff7DD97B);
+
+  static const orange = Color(0xffF3735E);
+}
