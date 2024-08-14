616a811e1deb0342f9692aa5039802ecea349ece
Ola2024Mohammed <hawaiiict2022@gmail.com>
2024-08-14 18:52:32 +0300

    add App Images .

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,31 @@
+class Assets {
+  Assets._();
+
+  static const String avatar1 = "assets/images/avatar_1.svg";
+
+  static const String avatar2 = "assets/images/avatar_2.svg";
+
+  static const String cardShape = "assets/images/card_shape.svg";
+
+  static const String dashboard = "assets/images/dashboard.svg";
+
+  static const String expenses = "assets/images/expenses.svg";
+
+  static const String income = "assets/images/income.svg";
+
+  static const String investments = "assets/images/investments.svg";
+
+  static const String logout = "assets/images/logout.svg";
+
+  static const String outcome = "assets/images/outcome.svg";
+
+  static const String personImage = "assets/images/person_image.svg";
+
+  static const String settings = "assets/images/settings.svg";
+
+  static const String statistics = "assets/images/statistics.svg";
+
+  static const String transactions = "assets/images/transactions.svg";
+
+  static const String wallet = "assets/images/wallet.svg";
+}
